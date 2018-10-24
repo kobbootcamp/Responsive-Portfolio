eb3e74cd5031b9b999d6575c22635d5f74da7f1f
kobbootcamp <kobbootcamp@gmail.com>
2018-10-24 18:12:51 -0500

    updated portfolio

---
+++ assets/javascript/script.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function () {
+
+
+    $(".pic-container").on("click", function () {
+
+
+        switch (this.id) {
+            case "word":
+            window.open("https://kobbootcamp.github.io/Word-Guess-Game/");
+                break;
+            case "starwars":
+            window.open("https://kobbootcamp.github.io/unit-4-game/");
+            break;
+            case "trivia":
+            window.open("https://kobbootcamp.github.io/TriviaGame/");
+            break;
+            case "farm":
+            window.open("https://kobbootcamp.github.io/GifTastic/");
+            break;
+        }
+    })
+
+
+})
