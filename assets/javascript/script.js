1f2f49ad18bc7461f1b83f3a1129d925db9e9813
kobbootcamp <kobbootcamp@gmail.com>
2018-11-01 19:23:07 -0500

    added train button

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,20 +4,22 @@ $(document).ready(function () {
 
     $(".pic-container").on("click", function () {
 
-
         switch (this.id) {
             case "word":
-            window.open("https://kobbootcamp.github.io/Word-Guess-Game/");
+                window.open("https://kobbootcamp.github.io/Word-Guess-Game/");
                 break;
             case "starwars":
-            window.open("https://kobbootcamp.github.io/unit-4-game/");
-            break;
+                window.open("https://kobbootcamp.github.io/unit-4-game/");
+                break;
             case "trivia":
-            window.open("https://kobbootcamp.github.io/TriviaGame/");
-            break;
+                window.open("https://kobbootcamp.github.io/TriviaGame/");
+                break;
             case "farm":
-            window.open("https://kobbootcamp.github.io/GifTastic/");
-            break;
+                window.open("https://kobbootcamp.github.io/GifTastic/");
+                break;
+            case "train":
+                window.open("https://kobbootcamp.github.io/TrainScheduler/");
+                break;
         }
     })
 
